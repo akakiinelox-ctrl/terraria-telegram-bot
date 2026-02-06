9ee7f678316842b4159b66edf2c1d4c9c8991cf7
akakiinelox-ctrl <akakiinelox@gmail.com>
2026-02-06 17:12:08 +0200

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,35 +1,22 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-
+# ---------- ГЛАВНОЕ МЕНЮ ----------
 def main_menu_kb():
-    kb = ReplyKeyboardMarkup(resize_keyboard=True)
-    kb.add(
+    return ReplyKeyboardMarkup(resize_keyboard=True).add(
         KeyboardButton("👁 Боссы"),
-        KeyboardButton("⭐ Избранное")
-    )
-    kb.add(
+        KeyboardButton("⭐ Избранное"),
         KeyboardButton("📊 Прогресс")
     )
-    return kb
 
-
+# ---------- БОССЫ ----------
 def bosses_kb():
     kb = ReplyKeyboardMarkup(resize_keyboard=True)
-
-    kb.add("🟢 👑 Король слизней")
-    kb.add("🟡 👁 Глаз Ктулху")
-    kb.add("🟡 🐛 Пожиратель миров")
-    kb.add("🟡 🧠 Мозг Ктулху")
-    kb.add("🟡 🐝 Королева пчёл")
-    kb.add("🔴 🦴 Скелетрон")
-    kb.add("🔴 🔥 Стена плоти")
-
-    kb.add("🏠 Главное меню")
-    return kb
-
-
-def back_menu_kb():
-    kb = ReplyKeyboardMarkup(resize_keyboard=True)
-    kb.add("👁 Боссы")
-    kb.add("🏠 Главное меню")
+    kb.add("🟢👑 Король слизней")
+    kb.add("🔴👁 Глаз Ктулху")
+    kb.add("🟡🐛 Пожиратель миров")
+    kb.add("🟣🧠 Мозг Ктулху")
+    kb.add("🟠🐝 Королева пчёл")
+    kb.add("⚪💀 Скелетрон")
+    kb.add("🔴🔥 Стена плоти")
+    kb.add("⬅ Назад")
     return kb
